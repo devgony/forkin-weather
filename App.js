8a23d1af8de82b80d992e4c19e373892c65f083b
devgony <devgony@gmail.com>
2020-09-11 00:28:59 +0900

    init weather

--- App.js
+++ App.js
@@ -13,10 +13,23 @@ export default class extends React.Component {
     isLoading: true,
   };
   getWeather = async (latitude, longitude) => {
-    const { data } = await axios.get(
+    const {
+      data,
+      data: {
+        main: { temp },
+        weather,
+        name,
+      },
+    } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
-    this.setState({ isLoading: false, temp: data.main.temp });
+    console.log(name);
+    this.setState({
+      isLoading: false,
+      temp,
+      condition: weather[0].main,
+      name: name,
+    });
   };
   getLocation = async () => {
     try {
@@ -24,7 +37,7 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
-      console.log(latitude, longitude);
+      // console.log(latitude, longitude);
       const obj = this.getWeather(latitude, longitude);
 
       // this.setState({ isLoading: false });
@@ -36,7 +49,11 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading, temp } = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
+    const { isLoading, temp, condition, name } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} name={name} />
+    );
   }
 }
--- Weather.js
+++ Weather.js
@@ -3,16 +3,36 @@ import { View, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
 import { getAppLoadingLifecycleEmitter } from "expo/build/launch/AppLoading";
 
-export default function Weather({ temp }) {
+export default function Weather({ temp, condition, name }) {
   return (
     <View style={styles.container}>
+      <Text>{name}</Text>
       <Text>{temp}</Text>
+      <Text>{condition}</Text>
     </View>
   );
 }
 
-Weather.propType = {
+Weather.propTypes = {
   temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Ash",
+    "Clear",
+    "Clouds",
+    "Drizzle",
+    "Dust",
+    "Fog",
+    "Haze",
+    "Main",
+    "Mist",
+    "Rain",
+    "Sand",
+    "Smoke",
+    "Snow",
+    "Squall",
+    "Thunderstorm",
+    "Tornado",
+  ]).isRequired,
 };
 
 const styles = StyleSheet.create({
